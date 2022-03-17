3a2513f1579173eb805869605390948ebe838d68
vertiond <vertion@protonmail.com>
2022-03-17 12:53:10 -0500

    Update codesigning keyfile

--- contrib/build-wine/sign.sh
+++ contrib/build-wine/sign.sh
@@ -5,7 +5,7 @@ test -n "$here" -a -d "$here" || exit
 cd $here
 
 CERT_FILE=${CERT_FILE:-~/codesigning/cert.crt}
-KEY_FILE=${KEY_FILE:-~/codesigning/key.pem}
+KEY_FILE=${KEY_FILE:-~/codesigning/vertcoin-project.p12}
 if [[ ! -f "$CERT_FILE" ]]; then
     ls $CERT_FILE
     echo "Make sure that $CERT_FILE and $KEY_FILE exist"
@@ -24,8 +24,8 @@ for f in $(ls *.exe); do
     echo "Signing $f..."
     osslsigncode sign \
       -h sha256 \
-      -certs "$CERT_FILE" \
-      -key "$KEY_FILE" \
+      -pkcs12 "$KEY_FILE" \
+      -pass "$VERTCOIN_PROJECT_KEY_PASSWORD" \
       -n "Electrum-VTC" \
       -i "https://vertcoin.org/" \
       -t "http://timestamp.digicert.com/" \
